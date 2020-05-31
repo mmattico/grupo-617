d5cb858f1bbade0f600a41fbf2babf6c97024ff7
Martin <martinmatticoli@gmail.com>
2020-05-31 11:10:35 -0300

    ver 0.2

---
+++ CODIGO/app/src/main/java/com/example/tp2/DatosUsr.java
@@ -0,0 +1,80 @@
+package com.example.tp2;
+
+public class DatosUsr {
+
+    private String name;
+    private String lastname;
+    private Integer dni;
+    private String email;
+    private String password;
+    private Integer commission;
+    private Integer group;
+
+    public DatosUsr(String name, String lastname, Integer dni, String email, String password, Integer commission, Integer group) {
+        this.name = name;
+        this.lastname = lastname;
+        this.dni = dni;
+        this.email = email;
+        this.password = password;
+        this.commission = commission;
+        this.group = group;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    public Integer getDni() {
+        return dni;
+    }
+
+    public void setDni(Integer dni) {
+        this.dni = dni;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Integer getCommission() {
+        return commission;
+    }
+
+    public void setCommission(Integer commission) {
+        this.commission = commission;
+    }
+
+    public Integer getGroup() {
+        return group;
+    }
+
+    public void setGroup(Integer group) {
+        this.group = group;
+    }
+
+
+}
--- CODIGO/app/src/main/java/com/example/tp2/Registro.java
+++ CODIGO/app/src/main/java/com/example/tp2/Registro.java
@@ -1,14 +1,30 @@
 package com.example.tp2;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Context;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
 import android.os.Bundle;
+import android.os.StrictMode;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.gson.Gson;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.ProtocolException;
+import java.net.URL;
 
 public class Registro extends AppCompatActivity {
     //// VARIABLES GLOBALES //////////////////////////////////////////////
@@ -40,10 +56,50 @@ public class Registro extends AppCompatActivity {
             if( validar_internet() )
             {
             ///// AGUANTE CHIKA
+                HttpURLConnection urlConnection = null;
+
+                try {
+
+                    DatosUsr body = new DatosUsr( this.nombre, this.apellido, Integer.valueOf(this.dni), this.email, this.password1, Integer.valueOf(this.comision), Integer.valueOf(this.grupo));
+                   // URL urlRegistrar = new URL("https://so-unlam.net.ar/api/api/register");
+                    Gson gson = new Gson();
+
+                    String json = gson.toJson(body);
+
+                  //  JSONObject jsonWS = new JSONObject();
+                   // jsonWS.put("url", urlRegistrar);
+                   // jsonWS.put("body", json);
+                    JSONObject jsonWS = new JSONObject(json);
+                    StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+                    StrictMode.setThreadPolicy(policy);
+
+                    urlConnection = (HttpURLConnection) new URL("http://so-unlam.net.ar/api/api/register").openConnection();
+                    urlConnection.setRequestMethod("POST");
+                    urlConnection.setRequestProperty("Content-Type", "application/json; charset=UTF-8");
+                    urlConnection.setDoOutput(true);
+                    urlConnection.setDoInput(true);
+                    urlConnection.setConnectTimeout(5000);
+
+                    DataOutputStream wr = new DataOutputStream((urlConnection.getOutputStream()));
+                    wr.write(jsonWS.toString().getBytes("UTF-8"));
+                    wr.flush();
+                    wr.close();
+
+                    String response = urlConnection.getResponseMessage();
+                    //getResponse(urlConnection);
+            } catch (ProtocolException e) {
+                    e.printStackTrace();
+                } catch (MalformedURLException e) {
+                    e.printStackTrace();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
             }
-        }
 
 
+    }
     }
 
     private boolean validar_internet ()
@@ -54,6 +110,22 @@ public class Registro extends AppCompatActivity {
 
     }
 
+    public static StringBuffer getResponse(HttpURLConnection urlConnection) throws IOException {
+        InputStream inStream = null;
+        inStream = urlConnection.getInputStream();
+        BufferedReader bReader = new BufferedReader(new InputStreamReader(inStream));
+
+        String inputLine;
+        StringBuffer response = new StringBuffer();
+
+        while ((inputLine = bReader.readLine()) != null) {
+            response.append(inputLine);
+        }
+
+        bReader.close();
+        inStream.close();
+        return response;
+    }
 
     private void cargar_campos()
     {
