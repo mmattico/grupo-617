9cd1c9e49c9ac50778c377f83f5a4162a4746d5e
Gabe <Gax_95@hotmail.com>
2020-06-03 17:33:22 -0300

    Terminado el Menu Principal

--- CODIGO/app/src/main/java/com/example/tp2/MenuPrincipal.java
+++ CODIGO/app/src/main/java/com/example/tp2/MenuPrincipal.java
@@ -2,7 +2,9 @@ package com.example.tp2;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MenuPrincipal extends AppCompatActivity {
 
@@ -10,5 +12,23 @@ public class MenuPrincipal extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu_principal);
+    }
+
+    public void irATapaPantallas (View view)
+    {
+        Intent llamador = new Intent(this, JuegoTapaPantalla.class);
+        startActivity(llamador);
+    }
+
+    public void irARollingColor (View view)
+    {
+        Intent llamador = new Intent(this, JuegoRoll.class);
+        startActivity(llamador);
+    }
+
+    public void irAHistorial (View view)
+    {
+        Intent llamador = new Intent(this, Historial.class);
+        startActivity(llamador);
     }
 }
